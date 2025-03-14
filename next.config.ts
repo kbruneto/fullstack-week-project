5c5675cbe3b3a638590fa858d606ea924f49af4d
kbruneto <hizztxt@gmail.com>
2025-03-14 01:31:49 -0300

    feat(images): add support for i.imgur.com in remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+    remotePatterns: [
+      { hostname: "u9a6wmr3as.ufs.sh" },
+      { hostname: "i.imgur.com" }
+    ]
+    
   },
 };
 
